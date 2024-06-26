cdd704966ccadd701ddabd3f4cfc7c58b14fad6c
Felipe Camacho <felipe_15camacho@hotmail.com>
2024-06-26 19:24:50 -0400

    first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON("package.json"),
-        less: {
+        pkg: grunt.file.readJSON('package.json'),
+        less:{
             development: {
-                files:{
-                    'dev/styles/main.css':'src/styles/main.less'
+                files: {
+                    'main.css': 'main.less'
                 }
             },
             production: {
@@ -12,94 +12,25 @@ module.exports = function(grunt) {
                     compress: true,
                 },
                 files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
-                }
-            }
-        },
-        watch: {
-            less: {
-                files: ['src/styles/**/*.less'],
-                tasks:['less:development']
-            },
-            html: {
-                files: ['src/index.html'],
-                tasks: ['replace:dev']
-            }
-        },
-        replace: {
-            dev:{
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: '../src/scripts/main.js'
-                        }
-                    ]
-                },
-                files:[
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['src/index.html',],
-                        dest: 'dev/'
-                    }
-                ]
-            },
-            dist:{
-                options: {
-                    patterns: [
-                        {
-                            match: 'ENDERECO_DO_CSS',
-                            replacement: './styles/main.min.css'
-                        },
-                        {
-                            match: 'ENDERECO_DO_JS',
-                            replacement: './scripts/main.min.js'
-                        }
-                    ]
-                },
-                files:[
-                    {
-                        expand: true,
-                        flatten: true,
-                        src: ['prebuild/index.html',],
-                        dest: 'dist/'
-                    }
-                ]
-            }
-        },
-        htmlmin: {
-            dist: {
-                options: {
-                    removeComments: true,
-                    collapseWhitespace: true
-                },
-                files: {
-                    'prebuild/index.html': 'src/index.html'
+                    'main.css': 'main.less'
                 }
             }
         },
         uglify: {
             target: {
                 files: {
-                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                'dist/js/scripts.min.js': ['assets/js/*.js']
                 }
             }
-        },
-        clean: ['prebuild']
+        }
+
     })
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-replace');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-clean');
 
-    grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'uglify', 'clean']);
+
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+grunt.registerTask('default', ['less', 'uglify']);
+
 }
